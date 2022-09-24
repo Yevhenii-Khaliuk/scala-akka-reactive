34e8fc8e2ab777a03ab1e9bc68aacdbfdc3d386b
Yevhenii Khaliuk <yevhenii.khaliuk@gmail.com>
2022-09-24 12:39:29 +0200

    Key-Value Store: implementation without Persistence

--- kvstore/src/main/scala/kvstore/Replica.scala
+++ kvstore/src/main/scala/kvstore/Replica.scala
@@ -1,59 +1,105 @@
 package kvstore
 
-import akka.actor.{ OneForOneStrategy, PoisonPill, Props, SupervisorStrategy, Terminated, ActorRef, Actor }
-import kvstore.Arbiter._
-import akka.pattern.{ ask, pipe }
-import scala.concurrent.duration._
-import akka.util.Timeout
+import akka.actor.{Actor, ActorRef, Props}
+
 
 object Replica {
   sealed trait Operation {
     def key: String
+
     def id: Long
   }
+
   case class Insert(key: String, value: String, id: Long) extends Operation
+
   case class Remove(key: String, id: Long) extends Operation
+
   case class Get(key: String, id: Long) extends Operation
 
   sealed trait OperationReply
+
   case class OperationAck(id: Long) extends OperationReply
+
   case class OperationFailed(id: Long) extends OperationReply
+
   case class GetResult(key: String, valueOption: Option[String], id: Long) extends OperationReply
 
   def props(arbiter: ActorRef, persistenceProps: Props): Props = Props(new Replica(arbiter, persistenceProps))
 }
 
 class Replica(val arbiter: ActorRef, persistenceProps: Props) extends Actor {
+
+  import Arbiter._
   import Replica._
   import Replicator._
-  import Persistence._
-  import context.dispatcher
 
-  /*
-   * The contents of this actor is just a suggestion, you can implement it in any way you like.
-   */
-  
+
   var kv = Map.empty[String, String]
   // a map from secondary replicas to replicators
   var secondaries = Map.empty[ActorRef, ActorRef]
   // the current set of replicators
   var replicators = Set.empty[ActorRef]
 
+  var persistence: ActorRef = context.actorOf(persistenceProps)
 
-  def receive = {
-    case JoinedPrimary   => context.become(leader)
+  var expectedSeq = 0L
+
+  var idCounter = 0L
+
+  def nextId(): Long = {
+    val ret = idCounter
+    idCounter += 1
+    ret
+  }
+
+  def receive: Receive = {
+    case JoinedPrimary => context.become(leader)
     case JoinedSecondary => context.become(replica)
   }
 
   /* TODO Behavior for  the leader role. */
   val leader: Receive = {
-    case _ =>
+    case Insert(key, value, id) =>
+      kv += (key -> value)
+      sender ! OperationAck(id)
+    case Remove(key, id) =>
+      kv -= key
+      sender ! OperationAck(id)
+    case Get(key, id) =>
+      sender ! GetResult(key, kv.get(key), id)
+    case Replicas(replicas) =>
+      replicas - self diff secondaries.keySet foreach (secondary => {
+        val replicator = context.actorOf(Replicator.props(secondary))
+        replicators += replicator
+        secondaries += secondary -> replicator
+        kv foreach { case (key, value) => replicator ! Replicate(key, Some(value), nextId()) }
+      })
   }
 
   /* TODO Behavior for the replica role. */
   val replica: Receive = {
-    case _ =>
+    case Get(key, id) =>
+      sender ! GetResult(key, kv.get(key), id)
+    case Snapshot(key, valueOption, seq) =>
+      seq match {
+        case receivedSeq if receivedSeq > expectedSeq => ()
+        case receivedSeq if receivedSeq < expectedSeq =>
+          expectedSeq = receivedSeq + 1
+          sender ! SnapshotAck(key, seq)
+        case _ => valueOption match {
+          case Some(value) =>
+            kv += (key -> value)
+            expectedSeq += 1
+            sender ! SnapshotAck(key, seq)
+          case None =>
+            kv -= key
+            expectedSeq += 1
+            sender ! SnapshotAck(key, seq)
+        }
+      }
   }
 
+  override def preStart(): Unit =
+    arbiter ! Join
 }
 
--- kvstore/src/main/scala/kvstore/Replicator.scala
+++ kvstore/src/main/scala/kvstore/Replicator.scala
@@ -1,44 +1,57 @@
 package kvstore
 
-import akka.actor.Props
-import akka.actor.Actor
-import akka.actor.ActorRef
-import scala.concurrent.duration._
+import akka.actor.{Actor, ActorRef, Props}
+
+import scala.concurrent.duration.DurationInt
 
 object Replicator {
   case class Replicate(key: String, valueOption: Option[String], id: Long)
+
   case class Replicated(key: String, id: Long)
-  
+
   case class Snapshot(key: String, valueOption: Option[String], seq: Long)
+
   case class SnapshotAck(key: String, seq: Long)
 
   def props(replica: ActorRef): Props = Props(new Replicator(replica))
 }
 
 class Replicator(val replica: ActorRef) extends Actor {
+
   import Replicator._
   import context.dispatcher
-  
-  /*
-   * The contents of this actor is just a suggestion, you can implement it in any way you like.
-   */
+
 
   // map from sequence number to pair of sender and request
   var acks = Map.empty[Long, (ActorRef, Replicate)]
   // a sequence of not-yet-sent snapshots (you can disregard this if not implementing batching)
   var pending = Vector.empty[Snapshot]
-  
+
+  context.system.scheduler.scheduleAtFixedRate(100.milliseconds, 100.milliseconds) { () =>
+    acks.toSeq.sortBy(_._1) foreach {
+      case (seq, (_, request)) => replica ! Snapshot(request.key, request.valueOption, seq)
+    }
+  }
+
   var _seqCounter = 0L
-  def nextSeq() = {
+
+  def nextSeq(): Long = {
     val ret = _seqCounter
     _seqCounter += 1
     ret
   }
 
-  
+
   /* TODO Behavior for the Replicator. */
   def receive: Receive = {
-    case _ =>
+    case Replicate(key, valueOption, id) =>
+      val seq = nextSeq()
+      acks += seq -> (sender, Replicate(key, valueOption, id))
+      replica ! Snapshot(key, valueOption, seq)
+    case SnapshotAck(key, seq) =>
+      val (leader, request) = acks(seq)
+      acks -= seq
+      leader ! Replicated(key, request.id)
   }
 
 }
